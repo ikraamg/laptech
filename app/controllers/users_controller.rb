bb157dc6136c0069a6952bf614db8e2bb60a23ee
Ikraam <ikraamghoor@gmail.com>
2020-10-13 20:01:51 +0200

    Update gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < ApplicationController
 
   # REGISTER
   def create
-    byebug
     @user = User.create(user_params)
     if @user.valid?
       token = encode_token({ user_id: @user.id })
