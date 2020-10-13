7dd1bea9fb5023e1935fb4b5ac3048bd18111ecb
Ikraam <ikraamghoor@gmail.com>
2020-10-13 21:54:53 +0200

    fixed linter errros - bug possible

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   def login
     @user = User.find_by(username: params[:username])
 
-    if @user && @user.authenticate(params[:password])
+    if @user&.authenticate(params[:password])
       token = encode_token({ user_id: @user.id })
       render json: { user: @user, token: token }
     else
