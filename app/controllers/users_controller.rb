76a94879a282fccad153b28f27de8f6226f943f3
Ikraam <ikraamghoor@gmail.com>
2020-10-13 17:48:13 +0200

    Add User Token Auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,52 +1,37 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show update destroy]
+  before_action :authorized, only: [:auto_login]
 
-  # GET /users
-  def index
-    @users = User.all
-
-    render json: @users
-  end
-
-  # GET /users/1
-  def show
-    render json: @user
-  end
-
-  # POST /users
+  # REGISTER
   def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: @user
+    byebug
+    @user = User.create(user_params)
+    if @user.valid?
+      token = encode_token({ user_id: @user.id })
+      render json: { user: @user, token: token }
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: { error: 'Invalid username or password' }
     end
   end
 
-  # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
+  # LOGGING IN
+  def login
+    @user = User.find_by(username: params[:username])
+
+    if @user && @user.authenticate(params[:password])
+      token = encode_token({ user_id: @user.id })
+      render json: { user: @user, token: token }
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: { error: 'Invalid username or password' }
     end
   end
 
-  # DELETE /users/1
-  def destroy
-    @user.destroy
+  def auto_login
+    render json: @user
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_user
-    @user = User.find(params[:id])
-  end
-
-  # Only allow a trusted parameter "white list" through.
   def user_params
-    params.require(:user).permit(:username, :password_digest, :email, :admin)
+    params.permit(:username, :password, :admin, :email)
   end
 end
