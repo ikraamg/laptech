8742152b5cf9b5ea0b9ad10184155fda48a4bc43
Ikraam <ikraamghoor@gmail.com>
2020-10-15 12:31:50 +0200

    Add status errors for json

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
       token = encode_token({ user_id: @user.id })
       render json: { user: @user, token: token }
     else
-      render json: { error: 'Invalid username or password' }
+      render json: { errors: 'Invalid username or password' }, status: 401
     end
   end
 
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
       token = encode_token({ user_id: @user.id })
       render json: { user: @user, token: token }
     else
-      render json: { error: 'Invalid username or password' }
+      render json: { error: 'Invalid username or password' }, status: 401
     end
   end
 
